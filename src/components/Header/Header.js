fea55ad2019553c64a06fcdb4c5fb91dcbe70090
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-11-04 11:20:09 -0600

    Change structure of header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <Link to='/' >
-      <h1 className='header'>☕️Cap Hill Coffee☕️</h1>
+      <h1 className='header'>☕️    Cap Hill Coffee    ☕️</h1>
     </Link>
   )
 }
